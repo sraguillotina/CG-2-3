7d818788fb829db209aa7b324cb0f6bdd981639f
Al-Khwarizmi <nfg214@icloud.com>
2018-07-20 23:33:27 -0500

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Class2_StringCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, there. What's your name?");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("What's your favorite season of the year," + " " + name + "?");
+            Console.Write("Season: ");
+            string season = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("What is your favorite activity during the " + season + "?");
+            Console.Write("Activity: ");
+            string activity = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine(name + " " + "likes" + " " + activity + " " + "during the" + " " + season + ".");
+            Console.ReadLine();
+
+
+        }
+    }
+}
